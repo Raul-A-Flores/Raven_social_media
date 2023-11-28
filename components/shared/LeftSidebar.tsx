b6e9cb780ad44c62639d7c633cce27349c35c2a1
Raul-A-Flores <raul.alexander.floress@gmail.com>
2023-11-28 14:16:59 -0800

    remove console log

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -25,7 +25,6 @@ function LeftSidebar(){
                     const isActive = (pathname.includes(link.route) && link.route.length > 1 || pathname === link.route)
 
                     if(link.route === '/profile') link.route =`${link.route}/${userId}`
-                    console.log(userId)
                     return(
                     <div>
                         <Link
